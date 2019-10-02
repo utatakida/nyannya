d7a96b179a2617969cb7e800eda7eb55cfc3669d
utatakida <nkc19.uta.takida@gmail.com>
2019-10-02 11:31:39 +0900

    プレハブ作成　敵キャラ生成バグあり

--- Assets/Script/SentouSceneScript/waniCon.cs
+++ Assets/Script/SentouSceneScript/waniCon.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class waniCon : MonoBehaviour
 {
+    public bool atari=false;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,16 @@ public class waniCon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(atari=false)
         GetComponent<Rigidbody2D>().velocity = new Vector2(1, 0);
+        
+    //    GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+
+    }
+    public void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "tankunekoPre")
+            atari = true;
+        Debug.Log("atari");
     }
 }
