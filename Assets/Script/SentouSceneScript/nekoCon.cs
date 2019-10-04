ee1474066e24a1db49402194852ee374c8cf5689
utatakida <nkc19.uta.takida@gmail.com>
2019-10-04 15:43:09 +0900

    猫とワニに攻撃とHPをもたせたにゃー

--- Assets/Script/SentouSceneScript/nekoCon.cs
+++ Assets/Script/SentouSceneScript/nekoCon.cs
@@ -4,29 +4,83 @@ using UnityEngine;
 
 public class nekoCon : MonoBehaviour
 {
+    //猫のコライダーが他のコライダーに接触したかのbool
     public bool atari;
-   
+
+ 
+    public int neko = 4200;
+
+    //ねこの攻撃時間を設定
+    public bool nekojikan;
+    
+    float jikan;
+    float kougekijikan;
+
+    //ワニのスクリプトを設定
+    waniCon wani = new waniCon();
+
+
+
+    
+
     // Start is called before the first frame update
     void Start()
     {
         atari = false;
+        jikan = 0;
+        kougekijikan = 2;
     }
 
     // Update is called once per frame
     void Update()
     {
+        jikan += Time.deltaTime;
+        if (jikan > kougekijikan)
+        {
+            nekojikan = true;
+            jikan = 0;
+        }
+        else
+            nekojikan = false;
+      /*  jikan += Time.deltaTime;
+        if (jikan > kougekijikan)
+        {
+            nekojikan = true;
+            jikan = 0;
+        }
+        else
+            nekojikan = false;*/
+
+        
+
          if (atari == true)
              GetComponent<Rigidbody2D>().velocity = new Vector2(0,0);
          if(atari==false)
              GetComponent<Rigidbody2D>().velocity = new Vector2(-0.45f, 0);
 
+        /*if (waniCon.wanijikan == true&&atari==true)
+        {
+            nekohp -= wani.waniPower;
+        }
+
+        if (nekohp < 0)
+        {
+            Destroy(gameObject);
+        }
+        Debug.Log(nekohp);*/
+
+        if (neko < 0)
+            Destroy(gameObject);
     }
 
-    public void OnTriggerEnter2D(Collider2D collision)
+    public void OnTriggerStay2D(Collider2D collision)
     {
+       
         if (collision.gameObject.name == "WaniAtarihantei")
         {
             atari = true;
+           
+            collision.transform.root.gameObject.GetComponent<waniCon>().wani -=500;
         }
     }
     
--- Assets/Script/SentouSceneScript/waniCon.cs
+++ Assets/Script/SentouSceneScript/waniCon.cs
@@ -4,20 +4,43 @@ using UnityEngine;
 
 public class waniCon : MonoBehaviour
 {
+    
+
+    public int wani=700;
+
+
+    //  ワニの攻撃時間を設定
+    public bool wanijikan;
+    
+    float jikan;
+    float kougekijikan;
+
     public bool atari=false;
-    //ワニの位置を共有するためpreivateにする
+    
    
     // Start is called before the first frame update
     void Start()
     {
-      
-       // GetComponent<SpriteRenderer>().sortingOrder = randamu;
+
+        jikan = 0;
+        kougekijikan = 2;
     }
 
     // Update is called once per frame
     void Update()
     {
-      
+        if (wani < 0)
+        {
+            Destroy(gameObject);
+        }
+       jikan += Time.deltaTime;
+        if (jikan > kougekijikan)
+        {
+            wanijikan = true;
+            jikan = 0;
+        }
+        else
+            wanijikan = false;
 
 
         if (atari == true)
@@ -27,16 +50,19 @@ public class waniCon : MonoBehaviour
           
             
         
-      
+        
 
     }
-    public void OnTriggerEnter2D(Collider2D collision)
+    public void OnTriggerStay2D(Collider2D collision)
     {
        
         if (collision.gameObject.name == "TankunekoAtarihantei")
             atari = true;
         if (collision.gameObject.name == "NekoAtarihantei")
+        {
             atari = true;
+            collision.transform.root.gameObject.GetComponent<nekoCon>().neko -= 700;
+        }
         if (collision.gameObject.name == "KimonekoAtarihantei")
             atari = true;
         
