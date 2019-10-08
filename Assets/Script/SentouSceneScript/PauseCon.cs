55687c2940b264f39f916948222f3e11779ca394
yuuhi1214 <kobayashiyu1214@icloud.com>
2019-10-08 15:26:54 +0900

    巨神猫のアニメーション作成あとは中断時のアニメーション作る

--- Assets/Script/SentouSceneScript/PauseCon.cs
+++ Assets/Script/SentouSceneScript/PauseCon.cs
@@ -5,15 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class PauseCon : MonoBehaviour
 {
+
+
+
     //　バックボタン
     [SerializeField]
     private GameObject reStartButton;
     [SerializeField]
     //戦闘離脱ボタン
     private GameObject ridatuButton;
-    //　最終確認パネル
-    [SerializeField]
-    private GameObject kakuPanel;
+  
     //バックパネル
     [SerializeField]
     private GameObject backPanel;
@@ -21,7 +22,25 @@ public class PauseCon : MonoBehaviour
     [SerializeField]
     private GameObject OPPanel;
 
+    //　最終確認バックパネル
+    [SerializeField]
+    private GameObject kakubackPanel;
+    //　最終確認パネル
+    [SerializeField]
+    private GameObject kakuPanel;
 
+    //　はいボタン
+    [SerializeField]
+    private GameObject yesButton;
+    [SerializeField]
+    //いいえボタン
+    private GameObject noButton;
+
+
+
+    
+
+    //中断ボタンを押したときの処理
     public void OnClick()
     {
         Time.timeScale = 0f;
@@ -30,6 +49,12 @@ public class PauseCon : MonoBehaviour
         kakuPanel.SetActive(false);
         reStartButton.SetActive(true);
         ridatuButton.SetActive(true);
+
+
+        kakuPanel.SetActive(false);
+        kakubackPanel.SetActive(false);
+        yesButton.SetActive(false);
+        noButton.SetActive(false);
 
     }
     //戻るときの処理
@@ -42,14 +67,51 @@ public class PauseCon : MonoBehaviour
         reStartButton.SetActive(false);
         ridatuButton.SetActive(false);
 
+        kakuPanel.SetActive(false);
+        kakubackPanel.SetActive(false);
+        yesButton.SetActive(false);
+        noButton.SetActive(false);
+
     }
     //戦闘離脱した際の処理
-    /*
     public void OnClick3()
     {
-      
-    }
-    */
+        backPanel.SetActive(true);
+        OPPanel.SetActive(true);
+        kakuPanel.SetActive(false);
+        reStartButton.SetActive(true);
+        ridatuButton.SetActive(true);
 
-   
+        kakuPanel.SetActive(true);
+        kakubackPanel.SetActive(true);
+        yesButton.SetActive(true);
+        noButton.SetActive(true);
+
+    }
+
+    //最終確認の処理 yesの時
+    public void OnClick4()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("nekokitiScene");//カッコ内のシーンに移動
+
+    }
+
+    //最終確認の処理 noの時
+    public void OnClick5()
+    {
+
+        backPanel.SetActive(true);
+        OPPanel.SetActive(true);
+        kakuPanel.SetActive(false);
+        reStartButton.SetActive(true);
+        ridatuButton.SetActive(true);
+
+
+        kakuPanel.SetActive(false);
+        kakubackPanel.SetActive(false);
+        yesButton.SetActive(false);
+        noButton.SetActive(false);
+    }
+
 }
